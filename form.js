8e004e48d98a562cc3c734b2314d50f02ee1eb5f
blas <blasmayor@gmail.com>
2020-10-14 16:00:14 +0200

    nuevo

--- form.js
+++ form.js
@@ -8,16 +8,19 @@ form.addEventListener('submit', function(){
     console.log('cargado')
 
     var name = document.querySelector('#name').value;
-    var surName = document.querySelector('#surname').value;
+    var sex = document.querySelector('#sex').value;
+    var weight = document.querySelector('#weight').value;
     var age = document.querySelector('#age').value;
     box_dashed.style.display= 'block'
 
     var p_name = document.querySelector('#p_name span')
-    var p_surname = document.querySelector('#p_surname span')
+    var p_sex = document.querySelector('#p_sex span')
+    var p_weight = document.querySelector('#p_weight span')
     var p_age = document.querySelector('#p_age span')
 
     p_name.innerHTML= name
-    p_surname.innerHTML= surName
+    p_sex.innerHTML= sex
+    p_weight.innerHTML= weight
     p_age.innerHTML= age
 
 
